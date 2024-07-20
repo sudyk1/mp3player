ec7e419dc348b16ef775be0eb7369edf7d9ff8e6
adams <108087334+sudyk1@users.noreply.github.com>
2024-07-20 15:43:51 +0200

    20.07.2024 initialized repository

---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module mp3player {
+    requires javafx.graphics;
+    exports pl.sudyk.mp3player.main to javafx.graphics;
+}
---
+++ src/main/java/pl/sudyk/mp3player/main/Main.java
@@ -0,0 +1,16 @@
+package pl.sudyk.mp3player.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("MP3 Player");
+        stage.show();
+    }
+}
