c62f760d2adc235105fdf4893b58d6e151e7c8e9
adams <108087334+sudyk1@users.noreply.github.com>
2024-07-23 14:19:14 +0200

    23.07.2024 created a scene in FXML

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module mp3player {
     requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
     exports pl.sudyk.mp3player.main to javafx.graphics;
 }
--- src/main/java/pl/sudyk/mp3player/main/Main.java
+++ src/main/java/pl/sudyk/mp3player/main/Main.java
@@ -1,6 +1,9 @@
 package pl.sudyk.mp3player.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +13,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        AnchorPane mainPane = FXMLLoader.load(getClass().getResource("/mainPane.fxml"));
+        Scene scene = new Scene(mainPane);
+        stage.setScene(scene);
         stage.setTitle("MP3 Player");
         stage.show();
     }
