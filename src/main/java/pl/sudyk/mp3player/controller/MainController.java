b578a43394d86c0cd6c48c00c9cf22476a3e7bc3
adams <108087334+sudyk1@users.noreply.github.com>
2024-07-31 12:34:16 +0200

    31.07.2024 added eventFilters

--- src/main/java/pl/sudyk/mp3player/controller/MainController.java
+++ src/main/java/pl/sudyk/mp3player/controller/MainController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
@@ -41,6 +42,25 @@ public class MainController {
 
     public void initialize() {
         System.out.println("MainController created");
+        configureVolume();
+        configureButtons();
     }
 
+    private void configureVolume() {
+        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, mouseEvent ->
+                System.out.println("Wciśnięto przycisk na suwaku głośności"));
+    }
+
+    private void configureButtons() {
+        previousButton.setOnAction(event -> System.out.println("Poprzednia piosneka"));
+        nextButton.setOnAction(event -> System.out.println("Następna piosenka"));
+
+        playButton.setOnAction(event -> {
+            if (playButton.isSelected()) {
+                System.out.println("Play");
+            } else {
+                System.out.println("Stop");
+            }
+        });
+    }
 }
